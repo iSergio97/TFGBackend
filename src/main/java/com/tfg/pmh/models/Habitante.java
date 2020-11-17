7ffa844fd9c2aca002a61d5f6b81fc6f1c2b3c20
Sergio <sergiotb15@gmail.com>
2020-11-17 13:57:51 +0100

    [E] - Nuevos métodos
    Añadido las anotaciones NoArgsControllers y AllArgsControllers a las entidades

--- src/main/java/com/tfg/pmh/models/Habitante.java
+++ src/main/java/com/tfg/pmh/models/Habitante.java
@@ -1,6 +1,8 @@
 package com.tfg.pmh.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,8 @@ import javax.validation.constraints.Pattern;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Habitante extends Persona {
 
     @Id
--- src/main/java/com/tfg/pmh/models/Identificador.java
+++ src/main/java/com/tfg/pmh/models/Identificador.java
@@ -1,6 +1,8 @@
 package com.tfg.pmh.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -14,6 +16,8 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Identificador {
 
     @Id
