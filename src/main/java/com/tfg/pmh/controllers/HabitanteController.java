67293bee2c21ac0d17a0d37d3e3ec1b5682f765c
Sergio <sergiotb15@gmail.com>
2020-12-15 21:28:25 +0100

    [E] - HabitanteController
    Edición clase de encriptado de texto.

--- src/main/java/com/tfg/pmh/controllers/HabitanteController.java
+++ src/main/java/com/tfg/pmh/controllers/HabitanteController.java
@@ -5,16 +5,17 @@ import com.tfg.pmh.services.HabitanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sun.security.provider.MD5;
-import sun.security.provider.SecureRandom;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
@@ -30,7 +31,7 @@ public class HabitanteController {
     private ResponseEntity<Integer> login(String username, String password) {
         try {
             Habitante habitante = habitanteService.findByUsername(username);
-            String hashedPassword = hashPassword(password, "ABCDE");
+            String hashedPassword = hashText(password);
             assert hashedPassword != null;
             if(hashedPassword.equals(habitante.getCuentaUsuario().getPassword())) {
                 return new ResponseEntity<>(HttpStatus.OK);
@@ -43,21 +44,10 @@ public class HabitanteController {
         }
     }
 
-    private String hashPassword(String password, String salt) throws NoSuchAlgorithmException {
-        try {
-            SecureRandom secureRandom = new SecureRandom();
-
-            PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 10, 512);
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("SHA512");
-            byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
-
-            //converting to string to store into database
-
-            return Base64.getMimeEncoder().encodeToString(hash);
-        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
-            System.out.println(e.getLocalizedMessage());
-            return null;
-        }
+    private String hashText(String text) {
+        int dureza = 5;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(dureza, new SecureRandom());
+        return encoder.encode(text);
     }
 
 }
