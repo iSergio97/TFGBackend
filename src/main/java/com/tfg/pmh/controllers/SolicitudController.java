364cd9a6d4ef3cce9e9319b107a285be81f44d66
Sergio <sergiotb15@gmail.com>
2021-01-14 19:46:47 +0100

    [E] - SolicitudService y Controller
    Se han eliminado referencias de imports en desusos en ambas clases.
    En el controlador, se ha añadido un método básico para guardar una Solicitud que recibe como parámetros una SolicitudForm.
    En el servicio se deconstruye de SolicitudForm a solicitud.

--- src/main/java/com/tfg/pmh/controllers/SolicitudController.java
+++ src/main/java/com/tfg/pmh/controllers/SolicitudController.java
@@ -1,13 +1,54 @@
 package com.tfg.pmh.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tfg.pmh.forms.SolicitudForm;
+import com.tfg.pmh.models.Habitante;
+import com.tfg.pmh.models.Respuesta;
+import com.tfg.pmh.models.Solicitud;
+import com.tfg.pmh.services.HabitanteService;
+import com.tfg.pmh.services.SolicitudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/solicitud")
 public class SolicitudController {
 
+    @Autowired
+    private HabitanteService habitanteService;
+
+    @Autowired
+    private SolicitudService service;
     // Métodos para los habitantes
+
+    //@RequestBody Habitante habitante
+    @PostMapping("/habitante/new")
+    public Respuesta prueba(@RequestBody SolicitudForm solicitud, BindingResult bindingResult) {
+        Respuesta respuesta = new Respuesta();
+        // Revisar qué status pondremos para BadRequest en el parámetro
+        respuesta.setStatus(350);
+        try {
+            Solicitud solicitudBD = this.service.deconstruct(solicitud);
+
+            // Error a la hora de guardar. Revisar mañana
+            this.service.save(solicitudBD);
+            respuesta.setObject(solicitudBD);
+            respuesta.setStatus(200);
+            return respuesta;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.err.println(e.getCause());
+            return respuesta;
+        }
+    }
+
+    @GetMapping("/h")
+    public SolicitudForm test() {
+        return new SolicitudForm();
+    }
 
     // Métodos para los administradores
 }
--- src/main/java/com/tfg/pmh/services/SolicitudService.java
+++ src/main/java/com/tfg/pmh/services/SolicitudService.java
@@ -1,17 +1,27 @@
 package com.tfg.pmh.services;
 
+import com.tfg.pmh.forms.SolicitudForm;
 import com.tfg.pmh.models.Solicitud;
 import com.tfg.pmh.repositories.SolicitudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.NoSuchElementException;
 
 @Service
 public class SolicitudService {
 
     @Autowired
     private SolicitudRepository repository;
+
+    @Autowired
+    private ViviendaService viviendaService;
+
+    @Autowired
+    private HabitanteService habitanteService;
 
     public void save(Solicitud solicitud) {
         assert solicitud != null;
@@ -27,6 +37,21 @@ public class SolicitudService {
         return this.repository.findSolicitudesBySolicitante(id);
     }
 
+    public Solicitud deconstruct(SolicitudForm solicitudForm) {
 
+        // En la solicitud no es donde se cambian los datos, es en la operación. Ahí se debe comprobar si hay silicitadoPor.
+        // Si lo hay, cambiar los datos del solicitadoPor
+        Solicitud solicitud = new Solicitud();
+        solicitud.setFecha(new Date());
+        solicitud.setSolicitante(this.habitanteService.findById(solicitudForm.getSolicitanteID()));
+        solicitud.setSolicitaPor(this.habitanteService.findById(solicitudForm.getSolicitaPorID()));
+        solicitud.setDni(solicitudForm.getDni());
+        solicitud.setNombre(solicitudForm.getNombre());
+        solicitud.setApellidos(solicitudForm.getApellidos());
+        solicitud.setViviendaNueva(viviendaService.findById(solicitudForm.getViviendaNuevaID()));
+        // Revisar si fuera necesario un parse
+        solicitud.setFecha(solicitudForm.getFechaNacimiento());
+        return solicitud;
+    }
 }
 
