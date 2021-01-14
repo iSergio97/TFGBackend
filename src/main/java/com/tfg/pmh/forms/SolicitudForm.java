7b7cacc3f2c3504794b97ac752336976be3db9d2
Sergio <sergiotb15@gmail.com>
2021-01-14 19:44:31 +0100

    [N] - SolicitudForm
    Creado paquete para nuevas clases Forms para las request de POST. Se debe crear una entidad Form por cada POST que haga el usuario al sistema.

---
+++ src/main/java/com/tfg/pmh/forms/SolicitudForm.java
@@ -0,0 +1,45 @@
+package com.tfg.pmh.forms;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tfg.pmh.models.Habitante;
+import com.tfg.pmh.models.Vivienda;
+import lombok.*;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class SolicitudForm {
+
+    @NotNull
+    private Long solicitanteID;
+
+    @NotNull
+    private Long solicitaPorID; // Campo añadido para la modificación de datos de menores por sus padres
+    
+    @NotBlank
+    private String dni;
+
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    private String apellidos;
+
+    @NotNull
+    private Long viviendaNuevaID;
+
+    @Past
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date fechaNacimiento;
+}
